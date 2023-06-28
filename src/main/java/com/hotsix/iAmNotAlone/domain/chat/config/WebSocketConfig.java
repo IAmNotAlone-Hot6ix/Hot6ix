087b502f31c055164bafd29f33a4aa8178c61a5d
badstar1004 <dmswjd4566@hanmail.net>
2023-06-28 10:40:32 +0900

    feat: STOMP JWT 토큰 유효성 검사 기능 추가
    - 간단한 유효성 검사 기능 추가

--- src/main/java/com/hotsix/iAmNotAlone/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/hotsix/iAmNotAlone/domain/chat/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.hotsix.iAmNotAlone.domain.chat.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -11,8 +12,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
+@Log4j2
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final FilterChannelInterceptor filterChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -26,12 +29,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메세지 발행 요청 url -> 메세지 보낼 때
         registry.setApplicationDestinationPrefixes("/pub");
+        // 메세지 구독 요청 url -> 메세지 받을 때
         registry.enableSimpleBroker("/sub");
     }
 
-//    @Override
-//    public void configureClientInboundChannel(ChannelRegistration registration) {
-//        registration.interceptors();
-//    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(filterChannelInterceptor);
+    }
 }
