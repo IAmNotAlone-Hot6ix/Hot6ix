ed7207a8f0b812ab1bf917c664e0ca8e3d62cf53
hikihun <skh140@naver.com>
2023-06-29 14:26:50 +0900

    refactor: 채팅방 리스트에서 마지막 채팅기록 가져오는 쿼리 수정

--- src/main/java/com/hotsix/iAmNotAlone/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/hotsix/iAmNotAlone/domain/chat/repository/ChatMessageRepository.java
@@ -2,7 +2,9 @@ package com.hotsix.iAmNotAlone.domain.chat.repository;
 
 import com.hotsix.iAmNotAlone.domain.chat.domain.ChatMessage;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,6 +14,6 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
     List<ChatMessage> findByChatRoomIdOrderByCreatedAt(Long id);
 
     // 채팅방 리스트에서 마지막 채팅 기록 가져오기
-    List<ChatMessage> findByChatRoomIdOrderByCreatedAtDesc(Long id);
+    Optional<ChatMessage> findFirstByChatRoomIdOrderByCreatedAtDesc(Long id);
 
 }
