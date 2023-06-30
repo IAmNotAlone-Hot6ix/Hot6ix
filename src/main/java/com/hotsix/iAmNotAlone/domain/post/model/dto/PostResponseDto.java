9ca908f4921477284afc857a27a98bab166c9fb4
badstar1004 <dmswjd4566@hanmail.net>
2023-07-01 00:44:13 +0900

    Refactor: responseDTO 수정, PostPageService 수정
    - responseDTO
       - 게시글 내용에 '더보기' 삭제
    - PostPageService
       - 주석처리 된 코드 삭제

--- src/main/java/com/hotsix/iAmNotAlone/domain/post/model/dto/PostResponseDto.java
+++ src/main/java/com/hotsix/iAmNotAlone/domain/post/model/dto/PostResponseDto.java
@@ -1,10 +1,9 @@
 package com.hotsix.iAmNotAlone.domain.post.model.dto;
 
 import com.hotsix.iAmNotAlone.domain.post.entity.Post;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -22,29 +21,17 @@ public class PostResponseDto {
     private boolean like;
 
     public static PostResponseDto of(Post post, Long commentCount, boolean likeFlag) {
-
         return PostResponseDto.builder()
                 .nickname(post.getMembership().getNickname())
                 .imgPath(post.getMembership().getImgPath())
                 .gender(post.getMembership().getGender())
                 .createdAt(post.getCreatedAt())
-                .content(removeContent(post.getContent()))
+                .content(post.getContent())
                 .commentCount(commentCount)
                 .postId(post.getId())
                 .postImgPath(post.getImgPath().get(0))
                 .address(post.getAddress())
                 .like(likeFlag)
                 .build();
-
     }
-
-    public static String removeContent(String content) {
-        if (content.length() > 30) {
-            content = content.substring(0, 30);
-            content += "...더보기";
-        }
-        return content;
-    }
-
-
 }
