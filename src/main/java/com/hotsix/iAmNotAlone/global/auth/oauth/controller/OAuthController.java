ee25610a27ab92b79e5c979b8401d0f57bff9480
InHo5389 <80667642+InHo5389@users.noreply.github.com>
2023-07-20 19:25:10 +0900

    Update OAuthController.java

--- src/main/java/com/hotsix/iAmNotAlone/global/auth/oauth/controller/OAuthController.java
+++ src/main/java/com/hotsix/iAmNotAlone/global/auth/oauth/controller/OAuthController.java
@@ -3,12 +3,14 @@ package com.hotsix.iAmNotAlone.global.auth.oauth.controller;
 import com.hotsix.iAmNotAlone.global.auth.oauth.dto.LoginResponseDto;
 import com.hotsix.iAmNotAlone.global.auth.oauth.service.OAuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OAuthController {
@@ -19,7 +21,7 @@ public class OAuthController {
     public ResponseEntity<LoginResponseDto> kakaoLogin(HttpServletRequest request) {
         String code = request.getParameter("code");
         String kakaoAccessToken = oauthService.getKakaoAccessToken(code).getAccess_token();
-
+        log.info("카카오톡 어세스 토큰 발급완료");
         return oauthService.kakaoLogin(kakaoAccessToken);
     }
 }
